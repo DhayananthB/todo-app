f0e4f1a031f247d011ed8426032638ca31ec8026
Dhayananth B <dhayananthtn@gmail.com>
2025-07-22 12:01:09 +0530

    small change

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTAC6Yw-FpCnvNwhu35q3DoNEQnur0b1Y",
-  authDomain: "my-project-1-7ca33.firebaseapp.com",
-  projectId: "my-project-1-7ca33",
-  storageBucket: "my-project-1-7ca33.firebasestorage.app",
-  messagingSenderId: "851621387416",
-  appId: "1:851621387416:web:42117bfde63dcca55a84ed",
-  measurementId: "G-D1FDS26GK8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
