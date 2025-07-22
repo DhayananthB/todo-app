30d4ce313bef9dbf1f6959d916ee4e452cfa4928
Dhayananth B <dhayananthtn@gmail.com>
2025-07-22 19:11:44 +0530

    changed small case to uppercase

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
-import AuthContext from "./Authcontext";
+import AuthContext from "./AuthContext";
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(undefined);
